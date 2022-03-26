417d40658bc9dfb7883cce859484e58d080957ed
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-26 15:39:41 +0300

    Fully Set Project Side Bar

--- app/partials/sidebar.php
+++ app/partials/sidebar.php
@@ -22,14 +22,25 @@
                 </a>
             </li>
             <li><a href="medical_records" class="ai-icon" aria-expanded="false">
-                    <i class="flaticon-381-diploma"></i>
+                    <i class="flaticon-381-notepad"></i>
                     <span class="nav-text">Medical Records</span>
                 </a>
             </li>
             <li><a href="bills" class="ai-icon" aria-expanded="false">
-                    <i class="flaticon-381-file"></i>
+                    <i class="flaticon-381-diploma"></i>
                     <span class="nav-text">Bills</span>
                 </a>
+            </li>
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-381-layer"></i>
+                    <span class="nav-text">Reports</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="reports_doctors">Doctors</a></li>
+                    <li><a href="reports_patients">Patients</a></li>
+                    <li><a href="reports_apponitments">Appointments</a></li>
+                    <li><a href="reports_bills">Bills</a></li>
+                </ul>
             </li>
         </ul>
     </div>
