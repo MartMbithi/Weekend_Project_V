0525ad1e9aa803bd837d62f65bd1b4b687bdac3c
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-26 13:57:20 +0300

    Implement Toaster Plug In

--- app/partials/scripts.php
+++ app/partials/scripts.php
@@ -1,5 +1,67 @@
- <!-- Required vendors -->
- <script src="../assets/vendor/global/global.min.js"></script>
- <script src="../assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
- <script src="../assets/js/custom.min.js"></script>
- <script src="../assets/js/deznav-init.js"></script>
+<!-- Required vendors -->
+<script src="../assets/vendor/global/global.min.js"></script>
+<script src="../assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+<script src="../assets/js/custom.min.js"></script>
+<script src="../assets/js/deznav-init.js"></script>
+<!-- Toastr -->
+<script src="../assets/plugins/toastr/toastr.min.js"></script>
+<!-- Custom File  -->
+<script src="../assets/js/bs-custom-file-input.min.js"></script>
+<!-- Data Tables CDN -->
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.25/datatables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
+<script>
+    /* Init Tool Tip Js */
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+    /* Init Data Tables */
+    $(document).ready(function() {
+        $('.table').DataTable();
+    });
+
+    $(document).ready(function() {
+        $('.report_table').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'csv', 'excel', 'pdf', 'print'
+            ]
+        });
+    });
+</script>
+<script>
+    /* Init Custom File Select */
+    $(document).ready(function() {
+        bsCustomFileInput.init();
+    });
+    /* Show Selected File Name */
+    document.querySelector('.custom-file-input').addEventListener('change', function(e) {
+        var fileName = document.getElementById("myInput").files[0].name;
+        var nextSibling = e.target.nextElementSibling
+        nextSibling.innerText = fileName
+    })
+</script>
+<!-- Init  Alerts -->
+<?php if (isset($success)) { ?>
+    <!-- Pop Success Alert -->
+    <script>
+        toastr.success('<?php echo $success; ?>')
+    </script>
+
+<?php }
+if (isset($err)) { ?>
+    <script>
+        toastr.error('<?php echo $err; ?>')
+    </script>
+<?php }
+if (isset($info)) { ?>
+    <script>
+        toastr.warning('<?php echo $info; ?>')
+    </script>
+<?php }
+?>
