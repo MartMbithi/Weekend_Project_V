f2fd4467eb1ffbfdf87735378dccf17fd3abf910
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-27 13:31:59 +0300

    Implement Doctor Profile - Update Login Info

--- views/doctor.php
+++ views/doctor.php
@@ -102,27 +102,23 @@ require_once('../app/partials/head.php');
                             <div class="card">
                                 <div class="card-body">
                                     <div class="media d-sm-flex d-block text-center text-sm-left pb-4 mb-4 border-bottom">
-                                        <img alt="image" class="rounded mr-sm-4 mr-0" width="130" src="images/avatar/1.jpg">
+                                        <img alt="image" class="rounded mr-sm-4 mr-0" width="130" src="../assets/images/no-profile.png">
                                         <div class="media-body align-items-center">
                                             <div class="d-sm-flex d-block justify-content-between my-3 my-sm-0">
                                                 <div>
-                                                    <h3 class="fs-22 text-black font-w600 mb-0">Cive Slauw</h3>
-                                                    <p class="mb-2 mb-sm-2">Check In date 21 August 2020, 12:45 AM</p>
+                                                    <h3 class="fs-22 text-black font-w600 mb-0"><?php echo $user->user_name; ?></h3>
+                                                    <p class="mb-2 mb-sm-2">Registered On <?php echo $user->user_date_added; ?></p>
                                                 </div>
-                                                <span>#P-00016</span>
+                                                <span>#<?php echo $user->user_number; ?></span>
                                             </div>
                                             <a href="javascript:void(0);" class="btn bgl-primary btn-rounded text-black mb-2 mr-2">
                                                 <svg class="mr-2 scale5" width="14" height="14" viewBox="0 0 26 26" fill="none" xmlns="../../www.w3.org/2000/svg.html">
                                                     <path d="M18 0.500061V3.00006H21.25L16.625 7.62506C15 6.25006 12.875 5.50006 10.5 5.50006C5 5.50006 0.5 10.0001 0.5 15.5001C0.5 21.0001 5 25.5001 10.5 25.5001C16 25.5001 20.5 21.0001 20.5 15.5001C20.5 13.1251 19.75 11.0001 18.375 9.37506L23 4.75006V8.00006H25.5V0.500061H18ZM10.5 23.0001C6.375 23.0001 3 19.6251 3 15.5001C3 11.3751 6.375 8.00006 10.5 8.00006C14.625 8.00006 18 11.3751 18 15.5001C18 19.6251 14.625 23.0001 10.5 23.0001Z" fill="#2BC155"></path>
                                                 </svg>
-                                                Male
+
+                                                <?php echo $user->user_age; ?> Years
                                             </a>
-                                            <a href="javascript:void(0);" class="btn bgl-primary btn-rounded mb-2 text-black">
-                                                <svg class="mr-2 scale5" width="14" height="14" viewBox="0 0 28 28" fill="none" xmlns="../../www.w3.org/2000/svg.html">
-                                                    <path d="M27.75 11.5C27.7538 10.8116 27.568 10.1355 27.213 9.54575C26.8581 8.95597 26.3476 8.47527 25.7376 8.15632C25.1276 7.83737 24.4415 7.69248 23.7547 7.73752C23.0678 7.78257 22.4065 8.01581 21.8434 8.4117C21.2803 8.80758 20.837 9.35083 20.5621 9.98192C20.2872 10.613 20.1913 11.3076 20.2849 11.9896C20.3785 12.6715 20.6581 13.3146 21.0929 13.8482C21.5277 14.3819 22.101 14.7855 22.75 15.015V19C22.75 20.6576 22.0915 22.2473 20.9194 23.4194C19.7473 24.5915 18.1576 25.25 16.5 25.25C14.8424 25.25 13.2527 24.5915 12.0806 23.4194C10.9085 22.2473 10.25 20.6576 10.25 19V17.65C12.3301 17.3482 14.2323 16.3083 15.6092 14.7203C16.9861 13.1322 17.746 11.1019 17.75 9V1.5C17.75 1.16848 17.6183 0.850537 17.3839 0.616116C17.1495 0.381696 16.8315 0.25 16.5 0.25H12.75C12.4185 0.25 12.1005 0.381696 11.8661 0.616116C11.6317 0.850537 11.5 1.16848 11.5 1.5C11.5 1.83152 11.6317 2.14946 11.8661 2.38388C12.1005 2.6183 12.4185 2.75 12.75 2.75H15.25V9C15.25 10.6576 14.5915 12.2473 13.4194 13.4194C12.2473 14.5915 10.6576 15.25 9 15.25C7.34239 15.25 5.75268 14.5915 4.58058 13.4194C3.40848 12.2473 2.75 10.6576 2.75 9V2.75H5.25C5.58152 2.75 5.89946 2.6183 6.13388 2.38388C6.3683 2.14946 6.5 1.83152 6.5 1.5C6.5 1.16848 6.3683 0.850537 6.13388 0.616116C5.89946 0.381696 5.58152 0.25 5.25 0.25H1.5C1.16848 0.25 0.850537 0.381696 0.616116 0.616116C0.381696 0.850537 0.25 1.16848 0.25 1.5V9C0.25402 11.1019 1.01386 13.1322 2.3908 14.7203C3.76773 16.3083 5.6699 17.3482 7.75 17.65V19C7.75 21.3206 8.67187 23.5462 10.3128 25.1872C11.9538 26.8281 14.1794 27.75 16.5 27.75C18.8206 27.75 21.0462 26.8281 22.6872 25.1872C24.3281 23.5462 25.25 21.3206 25.25 19V15.015C25.9792 14.7599 26.6114 14.2848 27.0591 13.6552C27.5069 13.0256 27.7483 12.2726 27.75 11.5Z" fill="#2BC155"></path>
-                                                </svg>
-                                                Diabetes
-                                            </a>
+
                                         </div>
                                     </div>
                                     <div class="row">
@@ -143,19 +139,11 @@ require_once('../app/partials/head.php');
                                                 </span>
                                                 <div class="media-body">
                                                     <span class="d-block text-light mb-2">Address</span>
-                                                    <p class="fs-18 text-dark">795 Folsom Ave, Suite 600 San Francisco, <strong>CADGE 94107</strong></p>
+                                                    <p class="fs-18 text-dark"><?php echo $user->user_address; ?></p>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-lg-6">
-                                            <div class="map-bx mb-3">
-                                                <img src="images/svg/map.svg" alt="">
-                                                <a href="javascript:void(0);" class="map-button">View in Fullscreen</a>
-                                                <a class="map-marker" href="#">
-                                                    <i class="las la-map-marker-alt"></i>
-                                                </a>
-                                            </div>
-                                        </div>
+
                                         <div class="col-lg-6 mb-lg-0 mb-3">
                                             <div class="media">
                                                 <span class="p-3 border border-primary-light rounded-circle mr-3">
@@ -165,7 +153,7 @@ require_once('../app/partials/head.php');
                                                 </span>
                                                 <div class="media-body">
                                                     <span class="d-block text-light mb-2">Phone</span>
-                                                    <p class="fs-18 text-dark font-w600 mb-0">+00 123 456 7680</p>
+                                                    <p class="fs-18 text-dark font-w600 mb-0"><?php echo $user->user_phone; ?></p>
                                                 </div>
                                             </div>
                                         </div>
@@ -179,7 +167,7 @@ require_once('../app/partials/head.php');
                                                 </span>
                                                 <div class="media-body">
                                                     <span class="d-block text-light mb-2">Email</span>
-                                                    <p class="fs-18 text-dark font-w600 mb-0"><a href="https://eres.dexignzone.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="1d74737b725d78657c707178337e7270">[email&#160;protected]</a></p>
+                                                    <p class="fs-18 text-dark font-w600 mb-0"><?php echo $user->user_email; ?></p>
                                                 </div>
                                             </div>
                                         </div>
@@ -190,133 +178,25 @@ require_once('../app/partials/head.php');
                         <div class="col-xl-3 col-xxl-12">
                             <div class="card abilities-chart">
                                 <div class="card-header border-0 pb-0">
-                                    <h4 class="fs-20 font-w600">Doctors Abilities</h4>
+                                    <h4 class="fs-20 font-w600">Update Password</h4>
                                 </div>
+                                <hr>
                                 <div class="card-body">
-                                    <div id="pie-chart" class="ct-chart ct-golden-section chartlist-chart"></div>
-                                    <div class="chart-point">
-                                        <div>
-                                            <span class="a"></span>
-                                            <span class="text-ov px-1 fs-15">Operation</span>
-                                        </div>
-                                        <div>
-                                            <span class="b"></span>
-                                            <span class="text-ov px-1 fs-15">Theraphy</span>
-                                        </div>
-                                        <div>
-                                            <span class="c"></span>
-                                            <span class="text-ov px-1 fs-15">Mediation</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-12">
-                            <div class="assigned-doctor2 owl-carousel">
-                                <div class="items">
-                                    <div class="media bg-white p-4 rounded align-items-center">
-                                        <img class="mr-3" src="images/doctors/6.jpg" alt="">
-                                        <div class="media-body d-block d-sm-flex  align-items-center justify-content-between">
-                                            <div class="mr-2">
-                                                <h4 class="fs-20"><a class="text-black" href="page-review.html">Glee Smiley</a></h4>
-                                                <p class="fs-14 mb-0"> I came with my mother, I was very nervous. But after entering here I felt warmed with smiling</p>
+                                    <form method="post" enctype="multipart/form-data" role="form">
+                                        <div class="row">
+                                            <div class="form-group col-md-12">
+                                                <label for="">Login Password</label>
+                                                <input type="password" required name="new_password" class="form-control">
                                             </div>
-                                            <div class="text-sm-right text-center mt-sm-0 mt-2">
-                                                <span class="badge badge-md badge-success fs-16 mb-2">4.5</span>
-                                                <div class="star-icons wspace-no">
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                </div>
+                                            <div class="form-group col-md-12">
+                                                <label for="">Confirm Login Password</label>
+                                                <input type="password" required name="confirm_password" class="form-control">
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="items">
-                                    <div class="media bg-white p-4 rounded align-items-center">
-                                        <img class="mr-3" src="images/doctors/7.jpg" alt="">
-                                        <div class="media-body d-block d-sm-flex  align-items-center justify-content-between">
-                                            <div class="mr-2">
-                                                <h4 class="fs-20"><a class="text-black" href="page-review.html">Stevani Anderson</a></h4>
-                                                <p class="fs-14 mb-0"> I came with my mother, I was very nervous. But after entering here I felt warmed with smiling</p>
-                                            </div>
-                                            <div class="text-sm-right text-center mt-sm-0 mt-2">
-                                                <span class="badge badge-md badge-success fs-16 mb-2">4.5</span>
-                                                <div class="star-icons wspace-no">
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                </div>
-                                            </div>
+                                        <div class="text-right">
+                                            <button type="submit" name="update_password" class="btn btn-success btn-roundedu">Submit</button>
                                         </div>
-                                    </div>
-                                </div>
-                                <div class="items">
-                                    <div class="media bg-white p-4 rounded align-items-center">
-                                        <img class="mr-3" src="images/doctors/8.jpg" alt="">
-                                        <div class="media-body d-block d-sm-flex  align-items-center justify-content-between">
-                                            <div class="mr-2">
-                                                <h4 class="fs-20"><a class="text-black" href="page-review.html">Cive Slauw</a></h4>
-                                                <p class="fs-14 mb-0"> I came with my mother, I was very nervous. But after entering here I felt warmed with smiling</p>
-                                            </div>
-                                            <div class="text-sm-right text-center mt-sm-0 mt-2">
-                                                <span class="badge badge-md badge-success fs-16 mb-2">4.5</span>
-                                                <div class="star-icons wspace-no">
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="items">
-                                    <div class="media bg-white p-4 rounded align-items-center">
-                                        <img class="mr-3" src="images/doctors/6.jpg" alt="">
-                                        <div class="media-body d-block d-sm-flex  align-items-center justify-content-between">
-                                            <div class="mr-2">
-                                                <h4 class="fs-20"><a class="text-black" href="page-review.html">Glee Smiley</a></h4>
-                                                <p class="fs-14 mb-0"> I came with my mother, I was very nervous. But after entering here I felt warmed with smiling</p>
-                                            </div>
-                                            <div class="text-sm-right text-center mt-sm-0 mt-2">
-                                                <span class="badge badge-md badge-success fs-16 mb-2">4.5</span>
-                                                <div class="star-icons wspace-no">
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="items">
-                                    <div class="media bg-white p-4 rounded align-items-center">
-                                        <img class="mr-3" src="images/doctors/7.jpg" alt="">
-                                        <div class="media-body d-block d-sm-flex  align-items-center justify-content-between">
-                                            <div class="mr-2">
-                                                <h4 class="fs-20"><a class="text-black" href="page-review.html">Glee Smiley</a></h4>
-                                                <p class="fs-14 mb-0">I came with my mother, I was very nervous. But after entering here I felt warmed with smiling</p>
-                                            </div>
-                                            <div class="text-sm-right text-center mt-sm-0 mt-2">
-                                                <span class="badge badge-md badge-success fs-16 mb-2">4.5</span>
-                                                <div class="star-icons wspace-no">
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                    <i class="las la-star"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
