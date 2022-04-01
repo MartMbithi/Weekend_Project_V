658d21bc6d789df930be771a752eb3326b207dd9
Martin Mbithi <martdevelopers254@gmail.com>
2022-04-01 14:17:38 +0300

    Intergrate Barcode

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '80f306fbf68a36fe3f23214ab9b1f19ed0312c8f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '80f306fbf68a36fe3f23214ab9b1f19ed0312c8f',
+            'dev_requirement' => false,
+        ),
+        'ayeo/gs1_barcode' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ayeo/gs1_barcode',
+            'aliases' => array(),
+            'reference' => 'd478e876ea057b4e9b5714374d350d7b9ae8e759',
+            'dev_requirement' => false,
+        ),
+    ),
+);
