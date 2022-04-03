e47795adfff462d8620979235f96a5c42644e836
Martin Mbithi <martdevelopers254@gmail.com>
2022-04-03 10:32:06 +0300

    Persist System Settings On Auth

--- views/confirm_password.php
+++ views/confirm_password.php
@@ -31,38 +31,45 @@ if (isset($_POST['reset_password'])) {
     }
 }
 require_once('../app/partials/head.php');
+/* Loads Settings */
+$ret = "SELECT * FROM  settings";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($settings = $res->fetch_object()) {
 ?>
 
-<body class="h-100">
-    <div class="authincation h-100">
-        <div class="container h-100">
-            <div class="row justify-content-center h-100 align-items-center">
-                <div class="col-md-6">
-                    <div class="authincation-content border border-success">
-                        <div class="row no-gutters">
-                            <div class="col-xl-12"><br>
-                                <h4 class="text-center mb-4">
-                                    <img src="../assets/images/logo.png">
-                                    <b> iAfya <br> Healthcare Information Management System </b>
-                                </h4>
-                                <hr>
-                                <div class="auth-form">
+    <body class="h-100">
+        <div class="authincation h-100">
+            <div class="container h-100">
+                <div class="row justify-content-center h-100 align-items-center">
+                    <div class="col-md-6">
+                        <div class="authincation-content border border-success">
+                            <div class="row no-gutters">
+                                <div class="col-xl-12"><br>
                                     <h4 class="text-center mb-4">
-                                        Confirm Password
+                                        <img src="../assets/images/logo.png">
+                                        <b> <?php echo $settings->sys_name; ?> <br> Healthcare Information Management System </b>
                                     </h4>
-                                    <form method="POST">
-                                        <div class="form-group">
-                                            <label class="mb-1"><strong> New Password</strong></label>
-                                            <input type="password" name="new_password" required class="form-control">
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="mb-1"><strong> Confirm New Password</strong></label>
-                                            <input type="password" name="confirm_password" required class="form-control">
-                                        </div>
-                                        <div class="text-center">
-                                            <button type="submit" name="reset_password" class="btn btn-primary btn-block">Confirm Password</button>
-                                        </div>
-                                    </form>
+                                    <hr>
+                                    <div class="auth-form">
+                                        <h4 class="text-center mb-4">
+                                            Confirm Password
+                                        </h4>
+                                        <form method="POST">
+                                            <div class="form-group">
+                                                <label class="mb-1"><strong> New Password</strong></label>
+                                                <input type="password" name="new_password" required class="form-control">
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="mb-1"><strong> Confirm New Password</strong></label>
+                                                <input type="password" name="confirm_password" required class="form-control">
+                                            </div>
+                                            <div class="text-center">
+                                                <button type="submit" name="reset_password" class="btn btn-primary btn-block">Confirm Password</button>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -70,14 +77,14 @@ require_once('../app/partials/head.php');
                 </div>
             </div>
         </div>
-    </div>
 
 
-    <!--**********************************
+        <!--**********************************
         Scripts
     ***********************************-->
-    <?php require_once('../app/partials/scripts.php'); ?>
+        <?php require_once('../app/partials/scripts.php'); ?>
 
-</body>
+    </body>
 
-</html>
+    </html>
+<?php } ?>
--- views/reset_password.php
+++ views/reset_password.php
@@ -20,43 +20,49 @@ if (isset($_POST['reset_password'])) {
     }
 }
 require_once('../app/partials/head.php');
+$ret = "SELECT * FROM  settings";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($settings = $res->fetch_object()) {
 ?>
 
-<body class="h-100">
-    <div class="authincation h-100">
-        <div class="container h-100">
-            <div class="row justify-content-center h-100 align-items-center">
-                <div class="col-md-6">
-                    <div class="authincation-content border border-success">
-                        <div class="row no-gutters">
-                            <div class="col-xl-12"><br>
-                                <h4 class="text-center mb-4">
-                                    <img src="../assets/images/logo.png">
-                                    <b> iAfya <br> Healthcare Information Management System </b>
-                                </h4>
-                                <hr>
-                                <div class="auth-form ">
+    <body class="h-100">
+        <div class="authincation h-100">
+            <div class="container h-100">
+                <div class="row justify-content-center h-100 align-items-center">
+                    <div class="col-md-6">
+                        <div class="authincation-content border border-success">
+                            <div class="row no-gutters">
+                                <div class="col-xl-12"><br>
                                     <h4 class="text-center mb-4">
-                                        Reset Password
+                                        <img src="../assets/images/logo.png">
+                                        <b> <?php echo $s->sys_name; ?> <br> Healthcare Information Management System </b>
                                     </h4>
-                                    <form method="POST">
-                                        <div class="form-group">
-                                            <label class="mb-1"><strong>Email</strong></label>
-                                            <input type="email" name="user_email" required class="form-control">
-                                        </div>
-                                        <div class="form-row d-flex justify-content-between mt-4 mb-2">
+                                    <hr>
+                                    <div class="auth-form ">
+                                        <h4 class="text-center mb-4">
+                                            Reset Password
+                                        </h4>
+                                        <form method="POST">
                                             <div class="form-group">
-                                                <div class="custom-control custom-checkbox ml-1">
+                                                <label class="mb-1"><strong>Email</strong></label>
+                                                <input type="email" name="user_email" required class="form-control">
+                                            </div>
+                                            <div class="form-row d-flex justify-content-between mt-4 mb-2">
+                                                <div class="form-group">
+                                                    <div class="custom-control custom-checkbox ml-1">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <a href="login">Remember Password?</a>
                                                 </div>
                                             </div>
-                                            <div class="form-group">
-                                                <a href="login">Remember Password?</a>
+                                            <div class="text-center">
+                                                <button type="submit" name="reset_password" class="btn btn-primary btn-block">Reset Password</button>
                                             </div>
-                                        </div>
-                                        <div class="text-center">
-                                            <button type="submit" name="reset_password" class="btn btn-primary btn-block">Reset Password</button>
-                                        </div>
-                                    </form>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -64,14 +70,14 @@ require_once('../app/partials/head.php');
                 </div>
             </div>
         </div>
-    </div>
 
 
-    <!--**********************************
+        <!--**********************************
         Scripts
     ***********************************-->
-    <?php require_once('../app/partials/scripts.php'); ?>
+        <?php require_once('../app/partials/scripts.php'); ?>
 
-</body>
+    </body>
 
-</html>
+    </html>
+<?php } ?>
