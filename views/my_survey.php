98c06366c97858865d7098d48e1872df5e7b14d8
Mart <41507748+MartMbithi@users.noreply.github.com>
2022-06-13 12:34:30 +0300

    Update my_survey.php

--- views/my_survey.php
+++ views/my_survey.php
@@ -20,6 +20,7 @@ if (isset($_POST['add_survey'])) {
     $survey_user_tests = $_POST['survey_user_tests'];
     $survey_user_vaccination = $_POST['survey_user_vaccination'];
 
+
     /* Persist Survey */
     $sql = "INSERT INTO surveys(survey_ref, survey_user_id, survey_user_dob, survey_user_gender, survey_syptoms, survey_other_difficulties,
     survey_user_ailments, survey_travel_history, survey_user_travel, survey_user_people_contacted, survey_user_fam_members, survey_user_tests,
@@ -42,8 +43,11 @@ if (isset($_POST['add_survey'])) {
         $survey_user_vaccination
     );
     $prepare->execute();
-    if ($prepare) {
-        $success = "Response Submitted";
+    /* Recomment Tests */
+    if ($prepare && $survey_user_tests == 'No' &&  $survey_user_vaccination == 'No') {
+        $success = "Response Submitted, Kindly Visit The Nearest Hospital For A Vaccination & Test";
+    } else if ($prepare && $survey_user_tests == 'Yes' && $survey_user_vaccination == 'Yes') {
+        $success = "Response Submitted, You Have Been Vaccinated!, Keep Safe";
     } else {
         $err = "Failed!, Please Try Again";
     }
